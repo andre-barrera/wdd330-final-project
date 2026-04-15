bd88e745f6a64bd53ab3c74721c77e3d3b0673ee
andre-barrera <andre.barrera.99@gmail.com>
2026-04-14 20:27:30 -0600

    updated changes

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,7 @@
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-  preview: {
-    host: '0.0.0.0',
-    port: process.env.PORT || 10000,
-    allowedHosts: 'all'
+  build: {
+    outDir: 'dist'
   }
 });
